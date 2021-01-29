a0b5278bb562354b9686929afa9422321d218bc3
fabrizio <faber.rvn@gmail.com>
2021-01-29 16:01:29 +0100

    #5 Dates

--- src/php/dates.php
+++ src/php/dates.php
@@ -1,3 +1,8 @@
 <?php
   echo "<h1>Dates</h1>";
+
+  echo "Today is " . date("d/m/Y") . "<br>";
+  echo "The current day is " . date("l") . "<br>";
+  echo "The current month is " . date("m") . "<br>";
+  echo "The current time is  " . date("h:i") . "<br>";
 ?>
