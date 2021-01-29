0f90c1264a3f1339660dac0f0b9ad3cf81e5f163
fabrizio <faber.rvn@gmail.com>
2021-01-29 16:41:09 +0100

    #8 Maths

--- src/php/maths.php
+++ src/php/maths.php
@@ -1,3 +1,18 @@
 <?php
   echo "<h1>Maths</h1>";
+
+  echo "<h2>Absolute Value</h2>";
+  echo(abs(-5));
+
+  echo "<h2>Rounded Highest Integer</h2>";
+  echo(round(3.2));
+
+  echo "<h2>Highest Value</h2>";
+  echo(max(0, 150, 30, 20, -8, -200));
+
+  echo "<h2>Lowest Value</h2>";
+  echo(min(0, 150, 30, 20, -8, -200));
+
+  echo "<h2>Random Number</h2>";
+  echo(rand());
 ?>
