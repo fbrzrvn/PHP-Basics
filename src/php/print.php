9b6ada9531b88860b3c10fe3f6c6234856de92a0
fabrizio <faber.rvn@gmail.com>
2021-01-29 12:33:03 +0100

    #2 Print

--- src/php/print.php
+++ src/php/print.php
@@ -2,9 +2,21 @@
   $username = "Mario";
   $names = array("Mario","Maria","Jack");
   echo "<h1>Print</h1>";
+
+  echo "<h2>echo</h2>";
+  echo '<p>echo: Hello $username</p>';
   echo "<p>echo: Hello $username</p>";
+  echo '<p>echo: Hello ' . $username . '</p>';
+
+  echo "<h2>print</h2>";
+  print '<p>print: Hello $username</p>';
   print "<p>print: Hello $username</p>";
-  print "<p>print: Hello $names[1] </p>";
+  print "<p>print: Hello " . $username . "</p>";
+  print "<p>true</p>";
+  print "<p>false</p>";
+
+  echo "<h2>print_r</h2>";
   print_r("<p> print_r: $username</p>");
-  print_r("print_r: $names");
+  print_r("<p>print_r: $names</p>");
+  print_r($names);
 ?>
