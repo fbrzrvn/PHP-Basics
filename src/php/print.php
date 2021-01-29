14ecd2617c0ccf1c5420c3dc556d18d02995bba3
fabrizio <faber.rvn@gmail.com>
2021-01-29 08:18:14 +0100

    #1 Info

---
+++ src/php/print.php
@@ -0,0 +1,10 @@
+<?php
+  $username = "Mario";
+  $names = array("Mario","Maria","Jack");
+  echo "<h1>Print</h1>";
+  echo "<p>echo: Hello $username</p>";
+  print "<p>print: Hello $username</p>";
+  print "<p>print: Hello $names[1] </p>";
+  print_r("<p> print_r: $username</p>");
+  print_r("print_r: $names");
+?>
